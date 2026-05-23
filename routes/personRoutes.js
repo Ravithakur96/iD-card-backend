211640b59be2e69e1c435e19b37e1810b238a0e7
Ravi Kumar <srbanda96@gmail.com>
2026-05-23 10:11:02 +0530

    added missing model

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const Person = require("../models/Person");
+const Person = require("../models/person");
 const axios = require("axios");
 const FormData = require("form-data");
 
